fad0eab68c72c91c34d6a669f94c5fcdb054ac6b
HIBA ALQAED <hibaalqaed@Hibas-New-MacBook-Air.local>
2020-12-28 18:22:28 +0300

    profileStore

--- src/stores/profileStore.js
+++ src/stores/profileStore.js
@@ -2,44 +2,31 @@ import instance from "./instance";
 import { makeAutoObservable } from "mobx";
 
 class profileStore {
-  trips = [];
+  profiles = [];
   loading = true;
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  //fetch
-  fetchTrips = async () => {
-    try {
-      const response = await instance.get("/trips");
-      this.trips = response.data;
-      console.log(
-        "🚀 ~ file: tripStore.js ~ line 16 ~ profileStore ~ fetchTrips= ~ this.trips",
-        this.trips
-      );
-      this.loading = false;
-    } catch (error) {
-      console.error("tripStore --> fetchTrips", error);
-    }
-  }; //end fetch
-
-  //edit trip
-  editTrip = async (updatedTrip) => {
+  // Edit Profile
+  editProfile = async (updatedProfile) => {
     try {
       const response = await instance.put(
-        `/trips/${updatedTrip.id}`,
-        updatedTrip
+        `/profiles/${updatedProfile.id}`,
+        updatedProfile
       );
 
-      const trip = this.trips.find((trip) => trip.id === updatedTrip.id);
-      for (const key in trip) trip[key] = updatedTrip[key];
+      const profile = this.profiles.find(
+        (profile) => profile.id === updatedProfile.id
+      );
+      for (const key in profile) profile[key] = updatedProfile[key];
     } catch (error) {
-      console.error("tripStore --> editTrip", error);
+      console.error("profileStore --> editProfile", error);
     }
-  }; //end edit trip
-} //end class
+  };
+}
 
-const tripStore = new profileStore();
-tripStore.fetchTrips();
-export default tripStore;
+const profileStore = new profileStore();
+profileStore.fetchTrips();
+export default profileStore;
