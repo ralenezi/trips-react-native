476e8962fee0601f7483ef31d78c04792335f8b3
HIBA ALQAED <hibaalqaed@Hibas-New-MacBook-Air.local>
2020-12-29 00:49:13 +0300

    EditProfileScreen

--- src/screens/profile/EditProfileScreen.js
+++ src/screens/profile/EditProfileScreen.js
@@ -18,14 +18,34 @@ import BackButton from "../../components/BackButton";
 //styles
 import { ProfileImage, ProfileBio, ProfileTrips } from "./styles";
 
-const ProfileScreen = ({ route, navigation }) => {
-  // const {profile } = route.params;
+const EditProfileScreen = ({ route, navigation }) => {
+  const { profile } = route.params;
+  const [profileEdit, setProfile] = useState(profile);
+
+  //add data to backend
+  const handleEdit = () => {
+    profileStore.editProfile(profileEdit);
+    navigation.navigate("ProfileScreen");
+  };
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
       <Title>Edit your profile</Title>
+      <TextInput
+        label="Image"
+        value={profileEdit.image}
+        onChangeText={(image) => setProfile({ ...profileEdit, image })}
+      />
+      <TextInput
+        label="Bio"
+        value={profileEdit.description}
+        onChangeText={(bio) => setProfile({ ...profileEdit, bio })}
+      />
+      <Button mode="contained" onPress={handleEdit}>
+        Edit
+      </Button>
     </Background>
   );
 };
 
-export default observer(ProfileScreen);
+export default EditProfileScreen;
